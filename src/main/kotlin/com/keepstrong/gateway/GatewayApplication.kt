7330a721683f7a68c5b6f6274067fc47957f09ad
Vinicius R <vrodrigues@alelo.com.br>
2022-09-18 14:44:43 -0300

    enabled eureka client

--- src/main/kotlin/com/keepstrong/gateway/GatewayApplication.kt
+++ src/main/kotlin/com/keepstrong/gateway/GatewayApplication.kt
@@ -2,8 +2,10 @@ package com.keepstrong.gateway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication
+@EnableEurekaClient
 class GatewayApplication
 
 fun main(args: Array<String>) {
